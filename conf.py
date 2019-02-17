b326616836960466a04eb8c02a84b7bbb0744e7d
Otto Winter <otto@otto-winter.com>
2019-02-17 13:46:01 +0100

    Revert

--- conf.py
+++ conf.py
@@ -122,7 +122,6 @@ html_theme_options = {
     'sidebar_collapse': True,
     'fixed_sidebar': True,
     'show_powered_by': False,
-    'font_size': '1.05em',
 }
 
 html_logo = 'images/logo-text.svg'
