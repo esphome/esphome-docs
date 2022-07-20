0f43ce10f7b0e524b4f767eeeb8a9a9fa57da909
Maurits <maurits@vloop.nl>
2022-07-21 01:09:10 +0200

    test result fix

--- _static/webserver-v1.js
+++ _static/webserver-v1.js
@@ -34,6 +34,7 @@ const actions = [
 const multi_actions = [
     ["select", "option"],
     ["number", "value"],
+    ["input_text", "value"],
     ];
 
 source.addEventListener('state', function (e) {
