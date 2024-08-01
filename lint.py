ebe5331355d5d6b1a74019d4d0ea32bd56d63e37
Guillem Pagès Gassull <git@guillempages.cat>
2024-08-01 11:03:31 +0200

    Move display components files one level up
    Though they are very closely related to display; they are components in their own right, and thus should be moved one level up.

--- lint.py
+++ lint.py
@@ -395,7 +395,7 @@ def lint_directive_formatting(fname, content):
     include=["*.rst"],
     exclude=[
         "components/web_server.rst",
-        "components/display/images.rst",
+        "components/images.rst",
     ],
 )
 def lint_esphome_io_link(fname, match):
